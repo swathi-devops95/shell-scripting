85a7d199273e2a2f40dee596af1c9cf9ff8b0c26
swathi <swathit964@gmail.com>
2023-11-24 09:34:50 +0530

    escpesequence.sh

---
+++ 02-escapesequence.sh
@@ -0,0 +1,28 @@
+ #!/bin/bash
+
+ # shebangnotation:This tells the script with interpreter  this script has to be executed.
+ # There can only be one interpreter per script and that should always be the first line of the script.
+
+  echo welcome to shell scripting
+
+
+  # This is how we execute the scripts:
+
+  # $ bash scriptname.sh or sh scriptname.sh or ./scriptname.sh
+
+
+  # \n : this indicated a new line
+  #  swathi\ndevops
+  #   swathi
+  #   devops
+
+  echo line1
+  echo line2
+  echo line3
+  
+  echo -e"line4\nline5"
+  # when ever you are using special characters always put them in doublequots
+  # " " :Double Quotes
+  #' '  :Single Quotes
+
+
