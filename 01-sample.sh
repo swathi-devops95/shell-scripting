78494a740338724135eb6e763fe585a69c33cac8
swathi <swathit964@gmail.com>
2023-11-24 08:36:58 +0530

    updated

--- 01-sample.sh
+++ 01-sample.sh
@@ -1,12 +1,14 @@
  #!/bin/bash
 
- # Shebangnotation:This tells the script with interpreter  this script has to be executed
- # There can only be one interpreter per script and that should always be the first line of the script
-  
+ # shebangnotation:This tells the script with interpreter  this script has to be executed.
+ # There can only be one interpreter per script and that should always be the first line of the script.
 
   echo welcome to shell scripting
 
 
-  # This is how we execute the scripts
+  # This is how we execute the scripts:
 
   # $ bash scriptname.sh or sh scriptname.sh or ./scriptname.sh
+
+  echo line1
+  echo line2
