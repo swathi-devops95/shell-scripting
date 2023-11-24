ee8bb1df689036977cf8a6f809c2c7e1873a5054
swathi <swathit964@gmail.com>
2023-11-24 11:39:24 +0530

    modified variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 # What is a variable???
-        A variable is something that holds the value which is dyanamic
+     #  A variable is something that holds the value which is dyanamic
 # variable is used to declare the values:a,b,c are variables which holds values 10,20,xyz
 
-    a=10                                     a is a integer
-    b=20                                     b is a integer      
-    c=xyz                                    c is a string.
+    a=10                                    # a is a integer
+    b=20                                    # b is a integer      
+    c=xyz                                   # c is a string.
 
 # IN LINUX/SHELL SCRIPTING THERE IS NO CONCEPT OF DATATYPES BY DEFAULT EVEYTHING IS A STRING
 
 # If your input or variaables are having special characters enclopse them in double quotes ""
 
 # How can i print the value of a variable????
-        Using special character we are going to print the value of a variable.
+      #  Using special character we are going to print the value of a variable.
 
 # $: this is the special charactr used to print the value of a variable
 
 # echo a    (gives you a)
 # echo $a   (gives u vlue assigned to a)
+
+echo a
 
 echo $a
