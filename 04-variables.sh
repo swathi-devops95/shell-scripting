b5b539807ed86f3d4d32b2d8f1e2022f9d4a4bf1
swathi <swathit964@gmail.com>
2023-11-24 11:36:57 +0530

    modified variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,9 +3,10 @@
 # What is a variable???
         A variable is something that holds the value which is dyanamic
 # variable is used to declare the values:a,b,c are variables which holds values 10,20,xyz
-    a=10----------; a is a integer
-    b=20----------; b is a integer      
-    c=xyz---------; c is a string.
+
+    a=10                                     a is a integer
+    b=20                                     b is a integer      
+    c=xyz                                    c is a string.
 
 # IN LINUX/SHELL SCRIPTING THERE IS NO CONCEPT OF DATATYPES BY DEFAULT EVEYTHING IS A STRING
 
