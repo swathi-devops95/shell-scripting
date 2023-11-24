3f903a4a680ab98862f0cd9a196e0f767f6f2afe
swathi <swathit964@gmail.com>
2023-11-24 07:06:17 +0530

    updated sample .sh with shebang

--- sample.sh
+++ sample.sh
@@ -1,1 +1,13 @@
-sample file
+ #!/bin/bash
+
+ # Shebangnotation:This tells the script with interpreter  this script has to be executed
+ # There can only be one interpreter per script and that should always be the first line of the script
+  
+
+  echo welcome to bash scripting (like print f command in c ,whatever we want to print we use echo)
+
+  result:basch scripting
+
+  # This is how we execute the scripts
+
+  # $ bash scriptname.sh or sh scriptname.sh or ./scriptname.sh
