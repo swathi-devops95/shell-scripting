5c9b5d1f8b090bcdf0615ad063a4683e24e7c45c
swathi <swathit964@gmail.com>
2023-11-24 10:22:45 +0530

    colours.sh

---
+++ 03-colours.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+# Each and every colour on a terminal you see will have a colour code and we need to use that code based on your need.
+
+# color       foreground      background
+# red               31              41
+# green             32              42
+# yellow            33              43
+# blue              34              44
+# magenta           35              45
+# cyan              36              46
+
+# syntax to print the color:
+# echo -e "\e[colorcodem  your message to be printed in colour \e[0m"
+# echo -e "\e[33m iam printing yellow colour \e[0m"
