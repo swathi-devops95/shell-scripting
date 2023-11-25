d616b39c671138c95ad1c2b35dd01751bcfa0fa8
swathi <swathit964@gmail.com>
2023-11-25 22:05:00 +0530

    created specialvariables.sh

---
+++ 06-specialvariables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Special variables give special properties to your variables
+# Here are few of the special variables : $0 TO $9 ,  $? ,  $# , $* , $@
+
+echo $0      # $0 prints th script nme of you ar executing
+
