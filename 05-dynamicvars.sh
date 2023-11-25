ad897c4c227ddd5a488de8ac69e2dc18abef278d
swathi <swathit964@gmail.com>
2023-11-25 22:22:33 +0530

    updated with escape sequence

--- 05-dynamicvars.sh
+++ 05-dynamicvars.sh
@@ -4,4 +4,5 @@ DATE="$(date +%F)"
 SESSIONS_COUNT="$( who |wc -l)"
 echo -e "todays date is \e[31m $DATE \e[0m"
 echo -e "total number of active sessions are \e[32m $SESSIONS_COUNT \e[0m"
-echo $0
+echo -e " executing file\t $0 "
+
