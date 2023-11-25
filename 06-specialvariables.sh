5e9ad1f7a056aa1e2f3faf74cab9118faac4d31a
swathi <swathit964@gmail.com>
2023-11-25 22:18:26 +0530

    added specialvariable to file

--- 06-specialvariables.sh
+++ 06-specialvariables.sh
@@ -3,5 +3,5 @@
 # Here are few of the special variables : $0 TO $9 ,  $? ,  $# , $* , $@
 
 echo $0      # $0 prints th script nme of you ar executing
-
+echo "executed script name is : $0"     # we are harcoding it
 
