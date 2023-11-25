0478546af7a97332031b0940872840901ecef59a
swathi <swathit964@gmail.com>
2023-11-25 21:34:18 +0530

    updated the file with color

--- 05-dynamicvars.sh
+++ 05-dynamicvars.sh
@@ -2,5 +2,5 @@
 
 DATE="$(date +%F)"
 SESSIONS_COUNT="$( who |wc -l)"
-echo "todays date is $DATE "
-echo "total number of active sessions $SESSIONS_COUNT"
+echo "todays date is \e[31m $DATE \e[0m"
+echo "total number of active sessions are \e[32m $SESSIONS_COUNT \e[0m"
