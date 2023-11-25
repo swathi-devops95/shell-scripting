e89c4086f8fb30721aaa4e4d96ff7078c3460266
swathi <swathit964@gmail.com>
2023-11-25 22:28:32 +0530

    added varible to file

--- 06-specialvariables.sh
+++ 06-specialvariables.sh
@@ -2,6 +2,8 @@
 # Special variables give special properties to your variables
 # Here are few of the special variables : $0 TO $9 ,  $? ,  $# , $* , $@
 
+ROCKET_NAME=CHANDRAYAN
 echo $0      # $0 prints th script nme of you ar executing
 echo "executed script name is : $0"     # we are harcoding it
+echo "Name of the recently launched rocket in india  is $ROCKET_NAME"   # WE ARE HARDCODING THIS ALSO
 
