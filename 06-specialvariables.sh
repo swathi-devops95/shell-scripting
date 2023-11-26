e667b60aa25214ab918a3a1a22be3b45307ab92e
swathi <swathit964@gmail.com>
2023-11-26 12:23:49 +0530

    auto push

--- 06-specialvariables.sh
+++ 06-specialvariables.sh
@@ -7,6 +7,7 @@ echo $0      # $0 prints th script nme of you ar executing
 echo "executed script name is : $0"     # we are harcoding it
 echo "Name of the recently launched rocket in india  is : $1"        # WE ARE HARDCODING THIS ALSO
 echo "Name of the popular car in states is : $2"
+echo "current topic is :$3"
 
  #bash scriptname.sh  arg1 arg2 arg3     (you can supply upto 9 arguments)
 
