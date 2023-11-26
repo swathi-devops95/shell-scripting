57c89ddcdeb97ee38d30674a1cbeaab88bdb8e08
swathi <swathit964@gmail.com>
2023-11-26 15:36:24 +0530

    added samole function to file

--- 09-promtvariables.sh
+++ 09-promtvariables.sh
@@ -3,4 +3,5 @@
 # whenever you are asked to enter your details that means breaking the script and ebtering promts comes into picture
 
 read -p "enter your name:" name     #whatever name you enter will be saved under a variable called name
+                                    #"enter your name" is a promt
 echo -e "name of the person who ran the script is $name"
---
+++ 10-commands.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#There are 4 type of commands avialable
+# 1) Binary                     (/bin ,/sbin)
+                                #/bin--commands which are saved inside the bin are regular commands.
+                                #/sbin--commands which are saved in sbin which is nothing but secure bin are the commands which can be invoked only by the root user.
+
+# 2) Aliases                    (Aliases are shortcuts, alias net="netstat -tulpn")
+
+# 3) Shell built -in commands   #commands which comes with the shell
+                                #ex-type cd ,type echo , type break 
+
+# 4) Functions
+            #functions are nothing but a set of commands that can be written in a sequence and cn be called n number of times as per 
+
+
+# how to declare a function?
+
+# This is how you declare a function 
+# f() {
+#     echo hai
+# }        
+
+# this is how we call a function
+# f
+
+#function is majorly used to group certain sort of things and call it whenever you want
+#function helps you in eliminating repetive set of codes n number of times
+
+sample() {
+    echo haifrom sample function
+    echo sample function is completed
+}
+sample
