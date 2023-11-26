9cea500a19ed77e2c400a80425c8e19267811bd2
swathi <swathit964@gmail.com>
2023-11-26 13:54:39 +0530

    auto push

--- 08-quotes.sh
+++ 08-quotes.sh
@@ -5,3 +5,12 @@ echo "$#"    # $# is going to print the number of arguments
 echo "$?"    # $? is going to print the exit code of the last command.
 echo "$*"   # $* is going to print the used variables        
 echo "$@"    # $* is going to print the used variables
+
+#Above will give you the values of special variables
+
+echo '$$'   # $$ is going to print the PID of the current proces 
+echo '$#'    # $# is going to print the number of arguments
+echo '$?'    # $? is going to print the exit code of the last command.
+echo '$*'   # $* is going to print the used variables        
+echo '$@'    # $* is going to print the used variables
+
