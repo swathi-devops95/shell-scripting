445e27ee429e2e4d1512a7c3b8dd77bd5e4182c9
swathi <swathit964@gmail.com>
2023-11-26 13:06:41 +0530

    auto push

--- 06-specialvariables.sh
+++ 06-specialvariables.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
+
 # Special variables give special properties to your variables
 # Here are few of the special variables : $0 TO $9 ,  $? ,  $# , $* , $@
 
- #ROCKET_NAME=CHANDRAYAN
+#ROCKET_NAME=CHANDRAYAN
+
+a=10
+b=20
+c=30
+echo value of a is :$a
+echo value of b is :$b
+echo value of c is :$c
 echo $0      # $0 prints th script nme of you ar executing
-echo $1
-echo $2
+
+
 echo "executed script name is : $0"     # we are harcoding it
-echo "Name of the recently launched rocket in india  is : $1"        # WE ARE HARDCODING THIS ALSO
+echo "Name of the recently launched rocket in india  is : $1"          
 echo "Name of the popular car in states is : $2"
 echo "current topic is :$3"
 
- #bash scriptname.sh  arg1 arg2 arg3     (you can supply upto 9 arguments)
+#bash scriptname.sh  arg1 arg2 arg3     (you can supply upto 9 arguments)
+
+#  bash scriptName.sh  arg1  arg2  arg3
+# bash arg1  arg2  arg3 arg4  arg5  arg6 arg7  arg8  arg9  arg10 
+#        1     2     3    4    5     6    7      8    9      10
+
+# bash scriptName.sh 100   200   300    ( like this you can supply a maximum of 9 variables from the command line)
+#                     $1    $2    $3  
+
+
+echo $$    # $$ is going to print the PID of the current proces 
+echo $#    # $# is going to print the number of arguments
+echo $?    # $? is going to print the exit code of the last command.
+echo $*    # $* is going to print the used variables
+echo $@    # $* is going to print the used variables
 
 
