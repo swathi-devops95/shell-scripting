f3ed4d87367cbf78c32eed3c2251ea86b7ebacbf
swathi <swathit964@gmail.com>
2023-11-26 12:14:44 +0530

    modified specialvariables

--- 06-specialvariables.sh
+++ 06-specialvariables.sh
@@ -2,9 +2,12 @@
 # Special variables give special properties to your variables
 # Here are few of the special variables : $0 TO $9 ,  $? ,  $# , $* , $@
 
-ROCKET_NAME=CHANDRAYAN
+ #ROCKET_NAME=CHANDRAYAN
 echo $0      # $0 prints th script nme of you ar executing
 echo "executed script name is : $0"     # we are harcoding it
-echo "Name of the recently launched rocket in india  is : $ROCKET_NAME"   # WE ARE HARDCODING THIS ALSO
- #echo "name of the recently launched rocket is $1 "
+echo "Name of the recently launched rocket in india  is : $1"        # WE ARE HARDCODING THIS ALSO
+ 
+
+ #bash scriptname.sh  arg1 arg2 arg3     (you can supply upto 9 arguments)
+ 
 
