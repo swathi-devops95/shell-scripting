a4f84c4f865ccd1c2719e5a7f6edc9ad967793b0
swathi <swathit964@gmail.com>
2023-11-29 21:14:05 +0530

    auto push

---
+++ 16-loops.sh
@@ -0,0 +1,13 @@
+#!/bib/bash
+#Loops are used to execute something for certain number of times
+
+
+# There are 2 major types of loops ; 
+#     1) For loop       ( When you know something to be executed n number of time, we use for loop)
+#     2) While loop     ( When you don't know something to be executed n number of times, we use while loop)
+
+
+# For loop syntax 
+for val in 10 20 30 40 50 ; do   
+    echo "Value for the loop is $val"
+done
