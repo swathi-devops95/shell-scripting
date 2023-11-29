75fd4e2fb3c5d142828a6795cb81e2d5db0ba262
swathi <swathit964@gmail.com>
2023-11-29 20:16:51 +0530

    auto push

--- 15-ifelse-condition.sh
+++ 15-ifelse-condition.sh
@@ -13,7 +13,7 @@
 
 echo "demo on if usage"
 
-ACTION=$1
+ACTION=$1                                       #"taking the arg from command"
 if [ "$ACTION" == "start" ] ; then
     echo "start payment"
     exit 0
@@ -22,3 +22,27 @@ else
     exit 1
 
 fi
+
+
+#case-3
+echo "demo on if usage"
+
+ACTION=$1
+if [ "$ACTION" == "start" ] ; then
+    echo "start payment"
+    exit 0
+
+else if [ "$ACTION" == "stop" ] ; then
+        echo "stop payment"
+        exit 1
+
+ else     if[ "$ACTION"== "restart" ] ; then
+        echo "restart payment"
+        exit 2
+
+
+else 
+        echo -e "\e[32m valid options are start or stop or restart \e[0m"
+        exit 3
+        
+    fi
