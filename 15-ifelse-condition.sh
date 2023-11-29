18f4e6a78dfaf34d13d9185367ad81300e62dc32
swathi <swathit964@gmail.com>
2023-11-29 20:02:36 +0530

    auto push

--- 15-ifelse-condition.sh
+++ 15-ifelse-condition.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+# echo "demo on if usage"
+
+# ACTION=$1
+# if [ "$ACTION" == "start" ] ;then
+#     echo "start payment"
+#     exit 0
+# fi
+
+
+#case-2
+
 echo "demo on if usage"
 
 ACTION=$1
-if [ "$ACTION" == "start" ] ;then
+if [ "$ACTION" == "start" ] ; then
     echo "start payment"
     exit 0
+else
+    echo "valid option is start only "
 fi
