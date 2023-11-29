603bc54679437ec5f852e2048cf545160b020002
swathi <swathit964@gmail.com>
2023-11-29 12:51:18 +0530

    auto puish

--- 09-read.sh
+++ 09-read.sh
@@ -6,4 +6,4 @@ read -p "enter your name:" name     #whatever name you enter will be saved under
                                     #"enter your name" is a promt
 echo -e "name of the person who ran the script is $name"
 
-## in the above line we used -e this e works only when we use escape sequence character otherwise it wont react if we mention aslo
+## in the above line we used -e this e works only when we use escape sequence character(\n \t) otherwise it wont react if we mention aslo
