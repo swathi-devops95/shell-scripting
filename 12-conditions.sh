7f922856c67f23563f53662f0cb9608655cdfa15
swathi <swathit964@gmail.com>
2023-11-29 12:06:01 +0530

    auto push

---
+++ 12-conditions.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+# Conditons helps us to execute something only if SOME-Factor is true and if that factor is false then that something won't be executed.
+
+# Syntax Of CASE 
+# case $var in 
+#     opt1)  commands-x ;; 
+#     opt2)  commands-y ;;
+# esac  
+
+
+ACTION =$1
+case $ ACTION in
+       start)
+            echo "starting payment service"
+            ;;
+        stop)
+            echo "stopping payment service" 
+            ;;
+        restart)
+            echo "restarting payment service"
+            ;;
+esac   
+
+# ACTION=$1
+
+# case $ACTION in 
+#     start)
+#         echo -e "\e[32m Starting Payment Service \e[0m"
+#         exit 0
+#         ;;
+#     stop)
+#         echo -e "\e[31m Stopping Payment Service \e[0m"
+#         exit 1
+#         ;;
+#     restart)
+#         echo -e "\e[33mRestarting Payment Service \e[0m"
+#         exit 2
+#         ;;
+#     *)
+#         echo -e "\e[35m Valid Options are start or stop or restart \e[0m"
+#         echo -e "\e[33m Example Usage \e[0m :\n \t bash scriptName stop"
+#         exit 3
+#         ;;
+# esac 
