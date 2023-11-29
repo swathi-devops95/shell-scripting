44caefeb13f9fd17e4a2a95719f4b9744fafbcb5
swathi <swathit964@gmail.com>
2023-11-29 12:49:24 +0530

    autopush

--- 09-promtvariables.sh
+++ 09-promtvariables.sh
@@ -5,3 +5,5 @@
 read -p "enter your name:" name     #whatever name you enter will be saved under a variable called name
                                     #"enter your name" is a promt
 echo -e "name of the person who ran the script is $name"
+
+## in the above line we used -e this e works only when we use escape sequence character otherwise it wont react if we mention aslo
