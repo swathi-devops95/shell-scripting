387e3262f0e249487114383afa901518ff5cd0e7
swathi <swathit964@gmail.com>
2023-11-29 13:00:29 +0530

    auto puh

--- 12-conditions.sh
+++ 12-conditions.sh
@@ -15,16 +15,20 @@ ACTION=$1
  case $ACTION in
         start)
             echo  -e "\e[32m Starting Payment Service \e[0m"
+            exit 0
             ;;
         stop)
              echo -e "\e[31m Stopping Payment Service \e[0m"
+             exit 1
             ;;
          restart)
              echo -e "\e[33m restarting Payment Service \e[0m"
+             exit 2
              ;;
           *)
           echo -e "\e[35m valid options are start or stop or restart \e[0m "
           echo -e " \e [33m example usage \e [0m: \n \t bash scriptname stop "
+          exit 3
           ;;
 esac        
 
