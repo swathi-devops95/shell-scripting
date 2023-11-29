83362da70122e89e08e6d7a924732fcaa6f754e9
swathi <swathit964@gmail.com>
2023-11-29 20:29:29 +0530

    auto puh

--- 15-ifelse-condition.sh
+++ 15-ifelse-condition.sh
@@ -32,17 +32,17 @@ if [ "$ACTION" == "start" ] ; then
     echo "start payment"
     exit 0
 
-else if [ "$ACTION" == "stop" ] ; then
+elif  [ "$ACTION" == "stop" ] ; then
         echo "stop payment"
         exit 1
 
- else if [ "$ACTION"== "restart" ] ; then
+ elif  [ "$ACTION"== "restart" ] ; then
         echo "restart payment"
         exit 2
 
 
 else 
         echo -e "\e[32m valid options are start or stop or restart \e[0m"
-        echo -e "\e[33m example usage \e[0m :\n \t bash scriptname stop \e[0m "
+        echo -e "\e[33m example usage \e[0m :\n \t bash scriptname stop "
         exit 3
     fi
