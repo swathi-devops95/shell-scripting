0842633ab56e03d8560edc6bc0752971c1168028
swathi <swathit964@gmail.com>
2023-11-30 12:14:45 +0530

    auto push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -4,7 +4,7 @@
 # validate the user who is running the script is a root user or not
 
 USER_ID=$(id -u)
-if [ if $USER_ID -ne 0 ]  then ;
+if [ $USER_ID -ne 0 ]  then ;
     echo -e "\e[31m script is expected to be executed by the root user or with a sudo privilige \e[0m \n \t Example: \n\t\t sudo bash wrappers.sh frontend"
     exit 1
 fi
