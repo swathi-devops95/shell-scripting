37e37f9d9372c2fa45e8010cce61d3d89bccf0bb
swathi <swathit964@gmail.com>
2023-12-08 12:25:43 +0530

    auto push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -35,7 +35,7 @@ systemctl start nginx   &>>  ${LOGFILE}
 stat $?
 
 echo -n "Downloading ${COMPONENT} component:"
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
 stat $?
 
 echo -n "Clean up of ${COMPONENT} :" 
