28a8e85bec18c95f47f49bc6bec1dbc0ac9e9dad
swathi <swathit964@gmail.com>
2023-12-08 14:59:56 +0530

    auto push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,3 +1,124 @@
 #!/bin/bash
 
-echo "I am mongodb"
+#echo "I am mongodb"
+# validate the user who is running the script is a root user or not
+
+ USER_ID=$(id -u)
+ COMPONENT=mongodb
+ LOGFILE="/tmp/${COMPONENT}.log"
+if [ $USER_ID -ne 0 ]  ; then
+    echo -e "\e[31m script is expected to be executed by the root user or with a sudo privilige \e[0m \n\t Example: \n\t\t sudo bash wrappers.sh frontend"
+    exit 1
+fi
+
+stat() {
+if [ $1 -eq 0 ] ; then
+    echo -e "\e[32m success \e[0m"
+else
+    echo -e "\e[31m failure \e[0m"
+    exit 2
+fi
+
+}
+
+echo -e "\e[31m  Configuring ${COMPONENT} .....! \e[0m \n"
+
+echo -e "Configuring ${COMPONENT} repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+stat $?
+
+echo -e "Installing ${COMPONENT}"
+yum install -y mongodb-org  &>>  ${LOGFILE}
+stat $?
+
+echo -e "Starting ${COMPONENT} :"
+systemctl enable ${COMPONENT}   &>> ${LOGFILE}
+systemctl start ${COMPONENT}    &>> ${LOGFILE}
+stat $?
+
+
+
+
+
+
+
+
+# yum install -y mongodb-org
+# systemctl enable mongod
+# systemctl start mongod
+# vim /etc/mongod.conf
+# systemctl restart mongod
+
+# Download the schema and inject it.
+ 
+# curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
+
+# cd /tmp
+# unzip mongodb.zip
+# cd mongodb-main
+# mongo < catalogue.js
+# mongo < users.js
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# echo -n  "installing nginx : "
+# yum install nginx -y  &>>  ${LOGFILE}
+# stat $?
+
+# echo -n -e "\e[33m  Starting nginx : \e[0m"
+# systemctl enable nginx  &>>  ${LOGFILE}
+# systemctl start nginx   &>>  ${LOGFILE}
+# stat $?
+
+# echo -n "Downloading ${COMPONENT} component:"
+# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+# stat $?
+
+# echo -n "Clean up of ${COMPONENT} :" 
+#  cd /usr/share/nginx/html
+#  rm -rf * &>>  ${LOGFILE}
+#  stat $?
+
+#  echo -n "Extracting ${COMPONENT} :"
+#  unzip /tmp/${COMPONENT}.zip  &>> ${LOGFILE}
+#  mv ${COMPONENT}-main/* .
+#  mv static/* .
+#  rm -rf ${COMPONENT}-main README.md
+#  mv localhost.conf /etc/nginx/default.d/roboshop.conf
+#  stat $?
+
+#  echo -n "Restarting ${COMPONENT}:"
+#  systemctl daemon-reload   &>> ${LOGFILE}
+#  systemctl restart nginx   &>> ${LOGFILE}
+#  stat $?
