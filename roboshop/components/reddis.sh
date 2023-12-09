39a977f32cb416a639a99c5c8cf5a8554e9cab7b
swathi <swathit964@gmail.com>
2023-12-09 18:55:49 +0530

    auto push

--- roboshop/components/reddis.sh
+++ roboshop/components/reddis.sh
@@ -1,3 +1,55 @@
 #!/bin/bash
 
-echo "I am reddis"
+#echo "I am reddis"
+
+# validate the user who is running the script is a root user or not
+
+ USER_ID=$(id -u)
+ COMPONENT=reddis
+ LOGFILE="/tmp/${COMPONENT}.log"
+
+ 
+if [ $USER_ID -ne 0 ]  ; then
+    echo -e "\e[31m script is expected to be executed by the root user or with a sudo privilige \e[0m \n\t Example: \n\t\t sudo bash wrappers.sh mongodb"
+    exit 1
+fi
+
+stat() {
+if [ $1 -eq 0 ] ; then
+    echo -e "\e[32m success \e[0m"
+else
+    echo -e "\e[31m failure \e[0m"
+    exit 2
+fi
+
+}
+
+echo -e "\e[31m  Configuring ${COMPONENT} .....! \e[0m \n"
+echo -n "Configuring ${COMPONENT} repo : "
+curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/${COMPONENT}.repo -o /etc/yum.${COMPONENT}.d/redis.repo
+
+ stat $?
+
+ echo -n  "Installing ${COMPONENT}:"
+ yum install redis-6.2.13 -y        &>> ${LOGFILE}
+ stat $?
+
+ echo -n "Enabling the ${COMPONENT} visibility :"
+ sed  -ie 's/127.0.0.1/0.0.0.0/g' /etc/${COMPONENT}.conf
+ stat $?
+
+ echo -n "Starting the ${COMPONENT}:"
+ systemctl daemon-reload         &>> ${LOGFILE} 
+ systemctl enable ${COMPONENT}   &>> ${LOGFILE}
+ systemctl start ${COMPONENT}    &>> ${LOGFILE}
+
+stat $?
+
+ echo -e "\e[32m  ${COMPONENT} Installation completed .....! \e[0m \n"
+
+
+
+
+
+# curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+# yum install redis-6.2.13 -y
