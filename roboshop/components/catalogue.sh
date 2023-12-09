f8635e0a606a934f02e724d284a48eecdee32c78
swathi <swathit964@gmail.com>
2023-12-09 14:46:21 +0530

    auto push

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "I am catalogue"
+
+USER_ID=$(ud -u)
+COMPONENT=catalogue
+LOGFILE="\tmp\${COMPONENT}.log"
+if [$USER_ID -ne 0] ; then
+    echo -e "\e[31m script is expected to be executed by root or with a sudo priviliged user \e[0m\n\t Example: \n\t sudo bash wrappers.sh catalogue "
+    exit 1
+fi
+    
