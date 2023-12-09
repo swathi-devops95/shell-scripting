c35c846b9c786db511c09dcb59419e04a903de02
swathi <swathit964@gmail.com>
2023-12-09 15:59:10 +0530

    auto push

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -60,60 +60,6 @@ stat $?
 
 
 
-# yum install nodejs -y  
-
-#  USER_ID=$(id -u)
-#  COMPONENT=frontend
-#  LOGFILE="/tmp/${COMPONENT}.log"
-# if [ $USER_ID -ne 0 ]  ; then
-#     echo -e "\e[31m script is expected to be executed by the root user or with a sudo privilige \e[0m \n\t Example: \n\t\t sudo bash wrappers.sh frontend"
-#     exit 1
-# fi
-
-# stat() {
-# if [ $1 -eq 0 ] ; then
-#     echo -e "\e[32m success \e[0m"
-# else
-#     echo -e "\e[31m failure \e[0m"
-#     exit 2
-# fi
-
-# }
-
-# # echo "congfiguring frontend"
-# echo -e "\e[31m  Configuring ${COMPONENT} .....! \e[0m \n"
-# echo -n  "installing nginx : "
-# yum install nginx -y  &>>  ${LOGFILE}
-# stat $?
-
-# echo -n -e "\e[33m  Starting nginx : \e[0m"
-# systemctl enable nginx  &>>  ${LOGFILE}
-# systemctl start nginx   &>>  ${LOGFILE}
-# stat $?
-
-# echo -n "Downloading ${COMPONENT} component:"
-# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-# stat $?
-
-# echo -n "Clean up of ${COMPONENT} :" 
-#  cd /usr/share/nginx/html
-#  rm -rf * &>>  ${LOGFILE}
-#  stat $?
-
-#  echo -n "Extracting ${COMPONENT} :"
-#  unzip /tmp/${COMPONENT}.zip  &>> ${LOGFILE}
-#  mv ${COMPONENT}-main/* .
-#  mv static/* .
-#  rm -rf ${COMPONENT}-main README.md
-#  mv localhost.conf /etc/nginx/default.d/roboshop.conf
-#  stat $?
-
-#  echo -n "Restarting ${COMPONENT}:"
-#  systemctl daemon-reload   &>> ${LOGFILE}
-#  systemctl restart nginx   &>> ${LOGFILE}
-#  stat $?
-
-#  echo -e "\e[31m  ${COMPONENT} Installation is completed  \e[0m \n"
 
 
 
