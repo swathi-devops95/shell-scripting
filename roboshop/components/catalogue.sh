f24ffde480d14349303d534dba903a825345891a
swathi <swathit964@gmail.com>
2023-12-09 14:50:49 +0530

    auto update

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -6,7 +6,7 @@ USER_ID=$(id -u)
 COMPONENT=catalogue
 LOGFILE="\tmp\${COMPONENT}.log"
 if [$USER_ID -ne 0] ; then
-    echo -e "\e[31m script is expected to be executed by root or with a sudo priviliged user \e[0m\n\t Example: \n\t sudo bash wrappers.sh catalogue "
+    echo -e "\e[31m script is expected to be executed by root or with a sudo priviliged user \e[0m \n\t Example: \n\t sudo bash wrappers.sh catalogue "
     exit 1
 fi
     
