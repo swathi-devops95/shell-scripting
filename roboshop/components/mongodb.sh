afcf0041899aec6bb599cd2de70c3f8c196459cc
swathi <swathit964@gmail.com>
2023-12-09 11:58:26 +0530

    auto push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -48,7 +48,7 @@ stat $?
 
  echo -n "Extracting the ${COMPONENT} schema :"
  cd /tmp
- unzip ${COMPONENT}.zip     &>> ${LOGFILE}
+ unzip -o ${COMPONENT}.zip     &>> ${LOGFILE}
  stat $?
 
  echo -n "Injecting ${COMPONENT} schema :"
