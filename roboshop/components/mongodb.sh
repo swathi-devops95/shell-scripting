d83b237d7bbb8de72e967f7e35d958e4429bef3b
swathi <swathit964@gmail.com>
2023-12-09 11:29:43 +0530

    auto push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -41,6 +41,43 @@ curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans
  systemctl start mongod   &>> ${LOGFILE}
 
 stat $?
+
+echo -n "Downloading the ${COMPONENT} schema : "
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+ stat $?
+
+ echo -n "Extracting the ${COMPONENT} schema :"
+ cd /tmp
+ unzip ${COMPONENT}.zip     &>> ${LOGFILE}
+ stat $?
+
+ echo -n "Injecting ${COMPONENT} schema :"
+ cd ${COMPONENT}-main
+ mongo < catalogue.js
+ mongo < users.js
+ STAT $?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # echo -e "Starting mongodb :"
 # systemctl enable mongodb  &>> /tmp/mongodb.log
 # systemctl start mongodb  &>> /tmp/mongodb.log
