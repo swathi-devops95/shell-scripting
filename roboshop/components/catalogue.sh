2092d8307e7a001bcc170debac42972977146da7
swathi <swathit964@gmail.com>
2023-12-09 15:57:43 +0530

    autopush

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -34,7 +34,7 @@ echo -n -e "\e[33m Installing nodejs : \e[0m"
 yum install nodejs -y       &>> ${LOGFILE}
 stat $?
 
-id ${APPUSER}
+id ${APPUSER}    &>> ${LOGFILE}
 if [ $? -ne 0 ]; then
 echo -n "Creating Application User Account :"
 useradd roboshop
