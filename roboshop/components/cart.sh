cf6840e6cd6e01cc88bc521d6e7d120b197b2374
swathi <swathit964@gmail.com>
2023-12-13 22:15:39 +0530

    auto push

--- roboshop/components/cart.sh
+++ roboshop/components/cart.sh
@@ -2,74 +2,83 @@
 
 #echo "I am cart"
 
-USER_ID=$(id -u)
 COMPONENT=cart
-LOGFILE="/tmp/${COMPONENT}.log"
-APPUSER="roboshop"
+#This is how we import the functions that are declared in a different files using source
+source components/common.sh
 
-if [ $USER_ID -ne 0 ] ; then
-    echo -e "\e[31m script is expected to be executed by root or with a sudo priviliged user \e[0m \n\t Example: \n\t sudo bash wrappers.sh cart"
-    exit 1
-fi
+NODEJS      #Calling nodejs function
 
-stat() {
-if [ $1 -eq 0 ] ; then
-    echo -e "\e[32m success \e[0m"
-else
-    echo -e "\e[31m failure \e[0m"
-    exit 2
-fi
-
-}
+echo -e "\e[32m  ${COMPONENT} Installation is completed  \e[0m \n"
 
 
-echo -e "\e[35m Configuring ${COMPONENT}......! \e[0m \n"
+# USER_ID=$(id -u)
+# COMPONENT=cart
+# LOGFILE="/tmp/${COMPONENT}.log"
+# APPUSER="roboshop"
 
-echo -n "configuring ${COMPONENT} repo : "
-curl --silent --location https://rpm.nodesource.com/setup_16.x |bash -   &>> ${LOGFILE}
-stat $?
+# if [ $USER_ID -ne 0 ] ; then
+#     echo -e "\e[31m script is expected to be executed by root or with a sudo priviliged user \e[0m \n\t Example: \n\t sudo bash wrappers.sh cart"
+#     exit 1
+# fi
 
-echo -n -e "\e[33m Installing nodejs : \e[0m"
-yum install nodejs -y    &>> ${LOGFILE}
-stat $?
+# stat() {
+# if [ $1 -eq 0 ] ; then
+#     echo -e "\e[32m success \e[0m"
+# else
+#     echo -e "\e[31m failure \e[0m"
+#     exit 2
+# fi
 
-id ${APPUSER}    &>> ${LOGFILE}
-if [ $? -ne 0 ]; then
-echo -n "Creating Application User Account :"
-useradd roboshop
-stat $?
-fi
+# }
 
-echo -n "Downloading the ${COMPONENT} : "
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
-stat $?
 
-echo -n "Copying the ${COMPONENT} to ${APPUSER} home directory"
-cd /home/${APPUSER}/
-rm -rf ${COMPONENT}   &>> ${LOGFILE}
-unzip -o /tmp/${COMPONENT}.zip   &>> ${LOGFILE}
-stat $?
+# echo -e "\e[35m Configuring ${COMPONENT}......! \e[0m \n"
 
-echo -n "changing the ownership :"
-mv ${COMPONENT}-main ${COMPONENT}
-chown -R ${APPUSER}:${APPUSER} /home/${APPUSER}/${COMPONENT}/
-stat $?
+# echo -n "configuring ${COMPONENT} repo : "
+# curl --silent --location https://rpm.nodesource.com/setup_16.x |bash -   &>> ${LOGFILE}
+# stat $?
 
-echo -n "Generating the ${COMPONENT} artifacts :"
-cd /home/${APPUSER}/${COMPONENT}/
-npm install    &>> ${LOGFILE}
-stat $?
+# echo -n -e "\e[33m Installing nodejs : \e[0m"
+# yum install nodejs -y    &>> ${LOGFILE}
+# stat $?
 
-echo -n "Configuring the ${COMPONENT} system file : "
-sed -ie 's/REDIS_ENDPOINT/redis.roboshop.internal/'  /home/${APPUSER}/${COMPONENT}/systemd.service
-sed -ie 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/'  /home/${APPUSER}/${COMPONENT}/systemd.service
-mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-stat $?
+# id ${APPUSER}    &>> ${LOGFILE}
+# if [ $? -ne 0 ]; then
+# echo -n "Creating Application User Account :"
+# useradd roboshop
+# stat $?
+# fi
 
-echo -n "Starting the ${COMPONENT} SERVICE :"
-systemctl daemon-reload
-systemctl enable ${COMPONENT}   &>> ${LOGFILE}
-systemctl restart ${COMPONENT}  &>> ${LOGFILE}
-stat $?
+# echo -n "Downloading the ${COMPONENT} : "
+# curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+# stat $?
 
- echo -e "\e[32m  ${COMPONENT} Installation is completed  \e[0m \n"
+# echo -n "Copying the ${COMPONENT} to ${APPUSER} home directory"
+# cd /home/${APPUSER}/
+# rm -rf ${COMPONENT}   &>> ${LOGFILE}
+# unzip -o /tmp/${COMPONENT}.zip   &>> ${LOGFILE}
+# stat $?
+
+# echo -n "changing the ownership :"
+# mv ${COMPONENT}-main ${COMPONENT}
+# chown -R ${APPUSER}:${APPUSER} /home/${APPUSER}/${COMPONENT}/
+# stat $?
+
+# echo -n "Generating the ${COMPONENT} artifacts :"
+# cd /home/${APPUSER}/${COMPONENT}/
+# npm install    &>> ${LOGFILE}
+# stat $?
+
+# echo -n "Configuring the ${COMPONENT} system file : "
+# sed -ie 's/REDIS_ENDPOINT/redis.roboshop.internal/'  /home/${APPUSER}/${COMPONENT}/systemd.service
+# sed -ie 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/'  /home/${APPUSER}/${COMPONENT}/systemd.service
+# mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+# stat $?
+
+# echo -n "Starting the ${COMPONENT} SERVICE :"
+# systemctl daemon-reload
+# systemctl enable ${COMPONENT}   &>> ${LOGFILE}
+# systemctl restart ${COMPONENT}  &>> ${LOGFILE}
+# stat $?
+
+#  echo -e "\e[32m  ${COMPONENT} Installation is completed  \e[0m \n"
--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -1,3 +1,42 @@
 #!/bin/bash
 
-echo "I am mysql"
+#echo "I am mysql"
+
+
+USER_ID=$(id -u)
+ COMPONENT=mysql
+ source components/common.sh
+ echo -e "\e[32m  ${COMPONENT} Installation is completed  \e[0m \n"
+
+ LOGFILE="/tmp/${COMPONENT}.log"
+
+ 
+if [ $USER_ID -ne 0 ]  ; then
+    echo -e "\e[31m script is expected to be executed by the root user or with a sudo privilige \e[0m \n\t Example: \n\t\t sudo bash wrappers.sh mongodb"
+    exit 1
+fi
+
+stat() {
+if [ $1 -eq 0 ] ; then
+    echo -e "\e[32m success \e[0m"
+else
+    echo -e "\e[31m failure \e[0m"
+    exit 2
+fi
+
+}
+
+echo -e "\e[31m  Configuring ${COMPONENT} .....! \e[0m \n"
+echo -n "Configuring ${COMPONENT} repo : "
+ curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo
+stat $?
+
+
+ echo -n  "Installing ${COMPONENT}:"
+ yum install mysql-community-server -y        &>> ${LOGFILE}
+ stat $?
+
+
+
+
+
