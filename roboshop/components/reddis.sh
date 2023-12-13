2e48c892c1da3c9283ea25693a79f383815936f1
swathi <swathit964@gmail.com>
2023-12-13 10:34:30 +0530

    auto push

--- roboshop/components/reddis.sh
+++ roboshop/components/reddis.sh
@@ -10,7 +10,7 @@
 
  
 if [ $USER_ID -ne 0 ]  ; then
-    echo -e "\e[31m script is expected to be executed by the root user or with a sudo privilige \e[0m \n\t Example: \n\t\t sudo bash wrappers.sh mongodb"
+    echo -e "\e[31m script is expected to be executed by the root user or with a sudo privilige \e[0m \n\t Example: \n\t\t sudo bash wrappers.sh reddis"
     exit 1
 fi
 
