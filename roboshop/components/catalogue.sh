471cab8ee8ca91358a65082acdceff1d86fd2473
swathi <swathit964@gmail.com>
2023-12-13 11:36:11 +0530

    auto push

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -61,7 +61,7 @@ npm install    &>> ${LOGFILE}
 stat $?
 
 echo -n "Configuring the ${COMPONENT} system file : "
-sed -ie 's/MONGO_DNSNAME/172.31.24.102/'  /home/${APPUSER}/${COMPONENT}/systemd.service
+sed -ie 's/MONGO_DNSNAME/mongodb.roboshop.internal/'  /home/${APPUSER}/${COMPONENT}/systemd.service
 mv /home/${APPUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
 stat $?
 
