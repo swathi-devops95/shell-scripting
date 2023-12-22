b2ac71d3c0d9057c938db4cc9c757aacbdb4b6ae
swathi <swathit964@gmail.com>
2023-12-22 10:46:12 +0530

    auto push

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -55,7 +55,7 @@ curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/sc
 stat $? 
 
 echo -n "Installing ${COMPONENT} :"
-yum install rabbitmq-server -y  &>> ${LOGFILE}
+yum install rabbitmq-server -y  &>> ${LOGFILE} # yum install rabbitmq-server -y
 stat $? 
 
 echo -n "Starting ${COMPONENT}:"
