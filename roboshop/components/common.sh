b48738fe1a52a426586f7dabf4fdf8c939bf713b
swathi <swathit964@gmail.com>
2023-12-22 06:34:12 +0530

    auto push

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -117,8 +117,7 @@ MVN_PACKAGE() {
     echo -n "Generating the ${COMPONENT} artifacts :"
     cd /home/${APPUSER}/${COMPONENT}/
     mvn clean package       &>> ${LOGFILE} 
-    mv ${COMPONENT}-main shipping
-    mv target/${COMPONENT}-1.0.jar${COMPONENT} .jar
+    mv target/${COMPONENT}-1.0.jar ${COMPONENT} .jar
     stat $?
 }
 
