1f6a50dd128e73c702f64121c8f44333d8fac10b
swathi <swathit964@gmail.com>
2023-12-22 11:11:10 +0530

    auto push

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -1,80 +1,38 @@
 #!/bin/bash
 
-LOGFILE="/tmp/${COMPONENT}.log"
-USER_ID=$(id -u)
+
+set -e
+# Validate the user who is running the script is a root user or not.
 COMPONENT=rabbitmq
 
+source components/common.sh
 
-if [ $USER_ID -ne 0 ] ; then
-    echo -e "\e[31m script is expected to be executed by root or with a sudo priviliged user \e[0m \n\t Example: \n\t sudo bash wrappers.sh ${COMPONENT}"
-    exit 1
-fi
+echo -e "\e[35m Configuring ${COMPONENT} ......! \e[0m \n"
 
-stat() {
-if [ $1 -eq 0 ] ; then
-    echo -e "\e[32m success \e[0m"
-else
-    echo -e "\e[31m failure \e[0m"
-    exit 2
-fi
+echo -n "Configuring ${COMPONENT} repositories:"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>> ${LOGFILE}
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash  &>> ${LOGFILE}
+stat $? 
 
-}
+echo -n "Installing ${COMPONENT} :"
+yum install rabbitmq-server -y  &>> ${LOGFILE}
+stat $? 
 
-#  source components/common.sh
+echo -n "Starting ${COMPONENT}:"
+systemctl enable rabbitmq-server   &>> ${LOGFILE}
+systemctl start rabbitmq-server    &>> ${LOGFILE}
+stat $? 
 
+sudo rabbitmqctl list_users | grep roboshop &>> ${LOGFILE}
+if [ $? -ne 0 ] ; then 
+    echo -n "Creating ${COMPOMENT} user account :"
+    rabbitmqctl add_user roboshop roboshop123 &>> ${LOGFILE}
+    stat $? 
+fi 
 
-echo -n  -e "\e[35m  configuring ${COMPONENT}.....! \e[0m \n"
-
-echo -n "configuring ${COMPONENT} repositories:"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash  &>> ${LOGFILE} 
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash     &>> ${LOGFILE}
+echo -n "Configuring the permissions :"
+rabbitmqctl set_user_tags roboshop administrator     &>> ${LOGFILE}
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"     &>> ${LOGFILE}
 stat $?
 
-echo -n "Installing ${COMPONENT}:"
-yum  install rabbitMQ-server -y       &>> ${LOGFILE}
-stat $?
-
-#  echo -n "starting ${COMPONENT}:"
-# systemctl enable rabbitmq-server 
-# systemctl start rabbitmq-server      &>> ${LOGFILE}
-# systemctl status rabbitmq-server     &>> ${LOGFILE} 
-#  stat $?
-
-# sudo rabbitmqctl list_users | grep roboshop     &>> ${LOGFILE}
-#     if [ $?  -ne  0 ] ; then
-# echo -n "creating ${COMPONENT} user account:"
-# rabbitmqctl add_user roboshop roboshop123      &>> ${LOGFILE}  
-# stat $?
-# fi
-
-# echo -n "Configuring the permissions:"
-# rabbitmqctl set_user_tags roboshop administrator
-# rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
-# stat $?
-
-
-
-#  echo -e "\e[32m  ${COMPONENT} Installation completed .....! \e[0m \n"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+echo -e "\e[35m ${COMPONENT} Installation Is Completed \e[0m \n"
