55f55b9f9382ae153dd3f56f40b45b2f6fd09f0f
swathi <swathit964@gmail.com>
2023-12-22 10:35:49 +0530

    auto push

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -7,8 +7,6 @@
  source components/common.sh
 
 
- echo -e "\e[32m  ${COMPONENT} Installation completed .....! \e[0m \n"
-
  echo -n "configiring ${COMPONENT} repositories:"
  curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash  &>> ${LOGFILE} 
  curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh |  bash     &>> ${LOGFILE}
@@ -26,7 +24,7 @@ systemctl status rabbitmq-server     &>> ${LOGFILE}
 stat $?
 
 sudo rabbitmqctl list_users | grep roboshop     &>> ${LOGFILE}
-    if [ $?  -ne  0 ];then
+    if [ $?  -ne  0 ] ;then
 echo -n "creating ${COMPONENT} user account:"
 rabbitmqctl add_user roboshop roboshop123      &>> ${LOGFILE}  
 stat $?
@@ -36,6 +34,10 @@ echo -n "Configuring the permissions:"
 rabbitmqctl set_user_tags roboshop administrator
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
 stat $?
+
+
+
+ echo -e "\e[32m  ${COMPONENT} Installation completed .....! \e[0m \n"
 
 
 
