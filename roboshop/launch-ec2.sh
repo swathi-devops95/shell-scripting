c153191f3cb085284bd96ad54a959b53dd567747
swathi <swathit964@gmail.com>
2024-01-04 13:28:13 +0530

    auto push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -20,7 +20,7 @@ INSTANCE_TYPE="t2.micro"
 SG_ID="$(aws ec2 describe-security-groups --filters Name=group-name,Values=B55_Allow_All | jq ".SecurityGroups[].GroupId" |sed -e 's/"//g')"
 HOSTEDZONEID="Z02387131OVDT30NOMVXF"
 
-
+create_ec2() {
 echo -e "**** Creating \e[35m ${COMPONENT} \e[0m server is in progress ****"
 
 PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' |sed -e 's/"//g')
@@ -36,6 +36,20 @@ sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADRESS/${PRIVATEIP}/" route53.json > 
 aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
 
 echo -e "\e[36m **** Creating DNS record for the $COMPONENT has completed ****\e[0m \n\n"
+
+}
+
+if [ "$1" == "all" ]; then
+
+    for component in frontend mongodb catalogue redis user mysql  cart rabbitmq shipping payment; do
+    COMPONENT=$component
+    create_ec2()
+    done
+
+else
+     create_ec2()
+
+fi
 
 
 
